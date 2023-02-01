5672762f77ea2976a8bff3a3d7b7bb5b26611b32
TonyChiu0101 <124105937+TonyChiu0101@users.noreply.github.com>
2023-02-01 08:40:29 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,6 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+git clone https://github.com/messense/aliyundrive-fuse.git package/aliyundrive-fuse
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+git clone https://github.com/linkease/docker_ddnsto.git package/docker_ddnsto
